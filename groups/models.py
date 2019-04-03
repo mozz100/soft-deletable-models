66e1b804ba451379e988bf0861db4bb1805ff5ff
Richard Morrison <richard@rmorrison.net>
2019-04-03 21:44:44 +0100

    Define __str__ for our models

--- groups/models.py
+++ groups/models.py
@@ -2,19 +2,23 @@ from django.db import models
 from model_utils.models import SoftDeletableModel
 
 
-class Group(models.Model):
+class NameMixin:
+    def __str__(self):
+        return self.name
+
+class Group(NameMixin, models.Model):
     name = models.CharField(max_length=127)
 
 
-class Musician(models.Model):
+class Musician(NameMixin, models.Model):
     name = models.CharField(max_length=127)
     groups = models.ManyToManyField(Group, related_name="members")
 
 
-class SoftDeletableGroup(SoftDeletableModel):
+class SoftDeletableGroup(NameMixin, SoftDeletableModel):
     name = models.CharField(max_length=127)
 
 
-class SoftDeletableMusician(SoftDeletableModel):
+class SoftDeletableMusician(NameMixin, SoftDeletableModel):
     name = models.CharField(max_length=127)
     groups = models.ManyToManyField(SoftDeletableGroup, related_name="members")
